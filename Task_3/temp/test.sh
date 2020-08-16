060fdd030bde588b873f87d3e09d3c62bcbd3e99
devansh-dvj <devansh.dvj@gmail.com>
2020-08-16 17:48:14 +0530

    Task 3: Completed wget part

---
+++ Task_3/temp/test.sh
@@ -0,0 +1,2 @@
+# wget -mk --no-parent -nH --cut-dirs=2 https://www.cse.iitb.ac.in/~vahanwala/CS251/verifier/
+wget -r -nH --no-parent --cut-dirs=3 https://www.cse.iitb.ac.in/~vahanwala/CS251/verifier/
---
+++ Task_3/verifier.sh
@@ -0,0 +1,2 @@
+wget -r -nH --no-parent --cut-dirs=3 https://www.cse.iitb.ac.in/~vahanwala/CS251/verifier/
+rm -rf *.html
