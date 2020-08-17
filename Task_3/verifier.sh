6073db0c1bec78fd5217f12f23d3635cc137f7a2
devansh-dvj <devansh.dvj@gmail.com>
2020-08-17 23:26:21 +0530

    Task 3: Fixed a bug with cut-dirs argument Followed by subsequent changes

--- Task_3/verifier.sh
+++ Task_3/verifier.sh
@@ -6,7 +6,7 @@ fi
 # Giving better names to the CLI arguments
 source_file="$1"
 url="$2"
-cut_dirs=$(($3 + 1))
+cut_dirs="$3"
 
 # Getting the test cases
 wget -q -r --no-parent --reject "index.html*" -nH --cut-dirs=${cut_dirs} ${url}
@@ -17,20 +17,21 @@ cpp_file=$(basename "${source_file}")
 g++ ${cpp_file}
 
 # Compute the output of the code for each input file
-mkdir my_outputs
-cd inputs
+cd $(basename ${url})
+mkdir -p my_outputs/
+cd inputs/
 for input in *.in; do
-    ../a.out <${input} >../my_outputs/${input%.in}.out
+    ../../a.out <${input} >../my_outputs/${input%.in}.out
 done
 
 # Now, comparing with the true output and saving the feedback
-echo "Failed testcases are:" >../feedback.txt
+echo "Failed testcases are:" >../../feedback.txt
 count=0
 for input in *.in; do
     difference=$(diff ../outputs/${input%.in}.out ../my_outputs/${input%.in}.out | wc -m)
     if [ ${difference} -ne 0 ]; then
         count=$((${count} + 1))
-        echo "${input%.in}" >>../feedback.txt
+        echo "${input%.in}" >>../../feedback.txt
     fi
 done
 
