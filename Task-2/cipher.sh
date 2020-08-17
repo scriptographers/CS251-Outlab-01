29e1189cf4fa0066cf20b6345ca743d2d2bbac0b
Harshit Varma <varmaharshit2@gmail.com>
2020-08-17 10:53:30 +0530

    Task-2: Completed

--- Task-2/cipher.sh
+++ Task-2/cipher.sh
@@ -6,26 +6,40 @@ then
     exit 1
 fi
 
+# Downloading the file from the given URL and storing it in 'encrypted.txt'
 URL=$1
-wget $URL -q -O encrypted.txt # -q hides the verbose present
+wget $URL -q -O encrypted.txt # -q hides the verbose present while running wget
 
 LOWER='abcdefghijklmnopqrstuvwxyz'
+SHIFT=1 # Specifies the Caeser shift
+E_LASTLINE=$( cat encrypted.txt | tail -1 ) # Stores the Encrypted Last Line, which is assumed to contain (encrypted) 'Queen', 'Mary', or 'Majesty'
 
-T_LOWER=''
-
-SHIFT=18
-for i in {0..25}
+while [ $SHIFT -le 25 ] # Brute-force attack on the last line
 do
-    INDEX=$(( (i+SHIFT)%26 ))
-    T_LOWER+=${LOWER:INDEX:1}
+	T_LOWER=''
+	for i in {0..25}
+	do
+	    INDEX=$(( (i+SHIFT)%26 ))
+	    T_LOWER+=${LOWER:INDEX:1}
+	done
+	UPPER=$( echo $LOWER | tr [:lower:] [:upper:] ) # Contains uppercase alphabets
+	T_UPPER=$( echo $T_LOWER | tr [:lower:] [:upper:] ) # Contains uppercase translated alphabets
+
+	ALPHABETS=$LOWER$UPPER # Entire english alphabet in both cases
+	TRANSLATION=$T_LOWER$T_UPPER # The entire translation
+	D_LASTLINE=$( echo $E_LASTLINE | tr $TRANSLATION $ALPHABETS ) # Decrypted Last Line
+	TREASON=$( echo $D_LASTLINE | egrep -i 'mary|queen|majesty' ) # Case-insensitive egrep search
+	if [ ${#TREASON} -gt 0 ] # We have found the right translation
+	then
+		break
+	else
+		(( SHIFT++ ))
+	fi
 done
 
-UPPER=$( echo $LOWER | tr [:lower:] [:upper:] )
-T_UPPER=$( echo $T_LOWER | tr [:lower:] [:upper:] )
+cat encrypted.txt | tr $TRANSLATION $ALPHABETS > deciphered.txt # Store the deciphered letter in 'deciphered.txt'
 
-ALPHABETS=$LOWER$UPPER
-TRANSLATION=$T_LOWER$T_UPPER
+echo "PS. Give me the names." | tr $ALPHABETS $TRANSLATION >> encrypted.txt # Encrypt and append the line to encrypted.txt 
 
-echo $TRANSLATION
+exit 0
 
-cat encrypted.txt | tr $ALPHABETS $TRANSLATION
